4b11b9c8db0b019b6d8bf95cbaecf2eed3fb6097
LoganneF <loganne.featherston@gmail.com>
2019-11-14 15:16:21 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,31 @@
 console.log("Up and running!")
 
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
-
 console.log("User flipped" + cardOne);
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+console.log("User flipped queen");
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+
 
 
 
